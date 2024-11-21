1780d1ea1aca2b4d7af2e551147091717aab3e70
lendysx16 <g.kurguzov@napopravku.ru>
2024-11-21 19:39:44 +0300

    change width for mobile

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -56,6 +56,9 @@ const modelValue = defineModel<boolean>("modelValue", { required: true, default:
     background-color: white;
     border-radius: 5px;
     box-shadow: 0 0 10px rgb(0 0 0 / 50%);
+    @media screen and (width < 768px) {
+        width: 80%;
+    }
 }
 
 .modal__title {
